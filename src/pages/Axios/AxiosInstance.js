bc01695998ecdb227a87da91f8ca435132f038d3
dev <shagen@pop-os.localdomain>
2024-05-08 12:01:35 +0500

    elishi.in

--- src/pages/Axios/AxiosInstance.js
+++ src/pages/Axios/AxiosInstance.js
@@ -1,24 +1,25 @@
 import axios from "axios";
 
-export const BASE_URL = "https://api.elishi.art/api/admin/";
+export const BASE_URL = "https://elishi.in/api/admin/";
+// export const BASE_URL = "http://95.85.121.153/api/admin/";
 const AxiosInstance = axios.create({
-    baseURL: BASE_URL,
-    timeout: 100000,
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('my_token')}`
-    }
-})
+  baseURL: BASE_URL,
+  timeout: 100000,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("my_token")}`,
+  },
+});
 
 const AxiosInstanceFormData = axios.create({
-    baseURL: BASE_URL,
-    timeout: 100000,
-    headers: {
-        'Authorization': `Bearer ${localStorage.getItem('my_token')}`,
-        'Content-Type': 'multipart/form-data'
-    }
-})
+  baseURL: BASE_URL,
+  timeout: 100000,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("my_token")}`,
+    "Content-Type": "multipart/form-data",
+  },
+});
 
-export const server_ip = "https://api.elishi.art";
-export {AxiosInstance};
-export {AxiosInstanceFormData};
+export const server_ip = "https://elishi.in";
+export { AxiosInstance };
+export { AxiosInstanceFormData };
